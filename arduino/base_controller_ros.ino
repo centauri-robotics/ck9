5358eb588c893421359abfa868feb0315e46c801
Jash Mota <jashmota101@gmail.com>
2021-02-23 11:25:52 +0530

    Update base_controller_ros.ino

--- arduino/base_controller_ros.ino
+++ arduino/base_controller_ros.ino
@@ -1,5 +1,6 @@
 /***
-   DC Motor-Encoder ROS Serial Communication Code
+   DC ROS Serial Communication Code
+   Author: Jash Mota
    Company: Centauri Robotics
 ***/
 #include <ros.h>
@@ -8,17 +9,13 @@
 
 ros::NodeHandle nh;
 // Left encoder
-
 int Left_Encoder_PinA = 2;
 int Left_Encoder_PinB = 9;
-
 volatile long Left_Encoder_Ticks = 0;
-
 //Variable to read current state of left encoder pin
 volatile bool LeftEncoderBSet;
 
 //Right Encoder
-
 int Right_Encoder_PinA = 3;
 int Right_Encoder_PinB = 10;
 volatile long Right_Encoder_Ticks = 0;
@@ -26,11 +23,11 @@ volatile long Right_Encoder_Ticks = 0;
 volatile bool RightEncoderBSet;
 
 // Left Motor
-int enA = 6;//3
+int enA = 6; //3
 int dir1 = 5;
 
 // Right Motor
-int enB = 11;//9
+int enB = 11; //9
 int dir2 = 8;
 
 //Initialising with defaults to keep the motors stopped
@@ -41,8 +38,9 @@ int leftanalog = 255, rightanalog = 255;
 //float rwheelv = 0;//right wheel velocity from twist_to_motors
 
 //Returns an Analog value for the given speed from twist to motors script
-int speedConv(float x, float in_min, float in_max, float out_min, float out_max) {
-  int analogval = (int( (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min));
+int speedConv(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  int analogval = (int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min));
   if (analogval < 0)
     analogval = 0;
   else if (analogval > 255)
@@ -57,12 +55,12 @@ void setleft(float val)
   {
     leftdir = 0;
     val = val * (-1);
-    leftanalog = speedConv(val, 0.0, 0.2, 0.0, 255.0);
+    leftanalog = speedConv(val, 0.0, 0.5, 0.0, 255.0);
   }
   else
   {
     leftdir = 1;
-    leftanalog = speedConv(val, 0.0, 0.2, 255.0, 0.0);
+    leftanalog = speedConv(val, 0.0, 0.5, 255.0, 0.0);
   }
 }
 
@@ -73,12 +71,12 @@ void setright(float val)
   {
     rightdir = 0;
     val = val * (-1);
-    rightanalog = speedConv(val, 0.0, 0.2, 0.0, 255.0);
+    rightanalog = speedConv(val, 0.0, 0.5, 0.0, 255.0);
   }
   else
   {
     rightdir = 1;
-    rightanalog = speedConv(val, 0.0, 0.2, 255.0, 0.0);
+    rightanalog = speedConv(val, 0.0, 0.5, 255.0, 0.0);
   }
 }
 
@@ -96,8 +94,9 @@ void leftmotor(const std_msgs::Float32 data) // 1 for clcckwise 2 for anti clock
   {
     digitalWrite(dir1, LOW);
   }
-  else {
-//    Serial.println(F("Wrong direction set for MA!!"));
+  else
+  {
+    //Serial.println(F("Wrong direction set for MA!!"));
   }
   analogWrite(enA, leftanalog);
 }
@@ -116,8 +115,9 @@ void rightmotor(const std_msgs::Float32 data) // 1 for clockwise 2 for anti cloc
   {
     digitalWrite(dir2, LOW);
   }
-  else {
-//    Serial.println(F("Wrong direction set for MB!!"));
+  else
+  {
+    //Serial.println(F("Wrong direction set for MB!!"));
   }
   analogWrite(enB, rightanalog);
 }
@@ -136,14 +136,14 @@ ros::Subscriber<std_msgs::Float32> subr("right_wheel_speed", &rightmotor);
 void setup()
 {
   //Initialise node and subscribe to necessary topics
-  nh.getHardware()->setBaud(57600);
+  // nh.getHardware()->setBaud(38400);
   nh.initNode();
   nh.subscribe(subl);
   nh.subscribe(subr);
   nh.advertise(pub1);
   nh.advertise(pub2);
-//  Serial.begin(57600);
-//  Serial.begin(38400);
+  Serial.begin(57600);
+  //  Serial.begin(38400);
   SetupEncoders();
   //Define motor pins as output pins
   pinMode(enA, OUTPUT);
@@ -156,14 +156,13 @@ void SetupEncoders()
 {
   // Quadrature encoders
   // Left encoder
-  pinMode(Left_Encoder_PinA, INPUT);      // sets pin A pullup
-  pinMode(Left_Encoder_PinB, INPUT);      // sets pin B pullup
+  pinMode(Left_Encoder_PinA, INPUT); // sets pin A pullup
+  pinMode(Left_Encoder_PinB, INPUT); // sets pin B pullup
   attachInterrupt(digitalPinToInterrupt(Left_Encoder_PinA), do_Left_Encoder, RISING);
 
-
   // Right encoder
-  pinMode(Right_Encoder_PinA, INPUT);      // sets pin A pullup
-  pinMode(Right_Encoder_PinB, INPUT);      // sets pin B pullup
+  pinMode(Right_Encoder_PinA, INPUT); // sets pin A pullup
+  pinMode(Right_Encoder_PinB, INPUT); // sets pin B pullup
 
   attachInterrupt(digitalPinToInterrupt(Right_Encoder_PinA), do_Right_Encoder, RISING);
 }
@@ -177,18 +176,17 @@ void loop()
   pub2.publish(&b);
 
   nh.spinOnce();
-//  delayMicroseconds(10);
+  //  delayMicroseconds(10);
   delay(10);
 }
 
 void do_Left_Encoder()
 {
-  LeftEncoderBSet = digitalRead(Left_Encoder_PinB);   // read the input pin
+  LeftEncoderBSet = digitalRead(Left_Encoder_PinB); // read the input pin
   Left_Encoder_Ticks += LeftEncoderBSet ? -1 : +1;
-
 }
 void do_Right_Encoder()
 {
-  RightEncoderBSet = digitalRead(Right_Encoder_PinB);   // read the input pin
+  RightEncoderBSet = digitalRead(Right_Encoder_PinB); // read the input pin
   Right_Encoder_Ticks += RightEncoderBSet ? +1 : -1;
 }
